a2fd34464fbefac8e9bafed117c588cfb145df43
Andrew Godin <covoxx@gmail.com>
2014-10-29 21:49:58 +0300

    fixes

--- lib/actions/shopMlmPluginFrontendSignup.action.php
+++ lib/actions/shopMlmPluginFrontendSignup.action.php
@@ -82,11 +82,161 @@ class shopMlmPluginFrontendSignupAction extends waSignupAction
     }
 
     /**
+     * @param array $data
+     * @param array $errors
+     * @return bool|waContact
+     */
+    public function signup($data, &$errors = array())
+    {
+        // check exists contacts
+        $auth = wa()->getAuth();
+        $field_id = $auth->getOption('login');
+        if ($field_id == 'login') {
+            $field_name = _ws('Login');
+        } else {
+            $field = waContactFields::get($field_id);
+            if ($field) {
+                $field_name = $field->getName();
+            } else {
+                $field_name = ucfirst($field_id);
+            }
+        }
+
+        $is_error = false;
+
+        // check passwords
+        if ($data['password'] !== $data['password_confirm']) {
+            $errors['password'] = array();
+            $errors['password_confirm'] = array(
+                _ws('Passwords do not match')
+            );
+            $is_error = true;
+        } elseif (!$data['password']) {
+            $errors['password'] = array();
+            $errors['password_confirm'][] = _ws('Password can not be empty.');
+            $is_error = true;
+        }
+
+        if (!$data[$field_id]) {
+            $errors[$field_id] = array(
+                sprintf(_ws("%s is required"), $field_name)
+            );
+            $is_error = true;
+        }
+        if (!$is_error) {
+            $contact = $auth->getByLogin($data[$field_id]);
+            if ($contact) {
+                $errors[$field_id] = array(
+                    sprintf(_ws('User with the same %s is already registered'), $field_name)
+                );
+                $is_error = true;
+            }
+        }
+
+        // set unconfirmed status for email
+        if (isset($data['email']) && $data['email']) {
+            $data['email'] = array('value' => $data['email'], 'status' => 'unconfirmed');
+        }
+
+        // check captcha
+        $auth_config = wa()->getAuthConfig();
+        if (isset($auth_config['signup_captcha']) && $auth_config['signup_captcha']) {
+            if (!wa()->getCaptcha()->isValid()) {
+                $errors['captcha'] = _ws('Invalid captcha');
+                $is_error = true;
+            }
+        }
+
+        if (is_array($auth_config['fields'])) {
+            foreach ($auth_config['fields'] as $fld_id => $fld) {
+                if (array_key_exists('required', $fld) && !$data[$fld_id] && $fld_id !== 'password') {
+                    $field = waContactFields::get($fld_id);
+                    if (!empty($fld['caption'])) {
+                        $field_name = $fld['caption'];
+                    } else if ($field) {
+                        $field_name = $field->getName();
+                    } else {
+                        $field_name = ucfirst($fld_id);
+                    }
+                    $errors[$fld_id] = array(
+                        sprintf(_ws("%s is required"), $field_name)
+                    );
+                    $is_error = true;
+                }
+            }
+        }
+
+
+        if ($is_error) {
+            return false;
+        }
+
+        if(isset($data['birthday']) && is_array($data['birthday']['value'])) {
+            foreach ($data['birthday']['value'] as $bd_id => $bd_val) {
+                if(strlen($bd_val) === 0) {
+                    $data['birthday']['value'][$bd_id] = null;
+                }
+            }
+        }
+
+        // remove password_confirm field
+        unset($data['password_confirm']);
+        // set advanced data
+        $data['create_method'] = 'signup';
+        $data['create_ip'] = waRequest::getIp();
+        $data['create_user_agent'] = waRequest::getUserAgent();
+        // try save contact
+        $contact = new waContact();
+
+        if (!$errors = $contact->save($data, true)) {
+            if (!empty($data['email'])) {
+                $this->send($contact);
+            }
+            // after sign up callback
+
+            //exit(12312);
+            $plugin = self::getPlugin();
+            $settings = $plugin->getSettings();
+            $storage = new waSessionStorage();
+
+            $mlm_id = $storage->get('mlm_id');
+            $settings['mlm_id'] = $mlm_id;
+            $plugin->saveSettings($settings);
+
+            $this->afterSignup($contact);
+
+            // try auth new contact
+            try {
+                if (wa()->getAuth()->auth($contact)) {
+                    $this->logAction('signup', wa()->getEnv());
+                }
+            } catch (waException $e) {
+                $errors = array('auth' => $e->getMessage());
+            }
+
+            return $contact;
+        }
+        if (isset($errors['name'])) {
+            $errors['firstname'] = array();
+            $errors['middlename'] = array();
+            $errors['lastname'] = $errors['name'];
+        }
+        return false;
+    }
+
+    public function signupUrl($absolute = false)
+    {
+        $shopUrl = wa()->getRouteUrl('shop/frontend');
+        $signup_url = $shopUrl . 'mlm/signup/';
+        return $signup_url;
+    }
+
+    /**
      * @param waContact $contact
      */
     protected function afterSignup(waContact $contact)
     {
-//        exit(12312);
+        exit(12312);
         $plugin = self::getPlugin();
         $settings = $plugin->getSettings();
         $storage = new waSessionStorage();
--- lib/actions/shopMlmPluginSettings.action.php
+++ lib/actions/shopMlmPluginSettings.action.php
@@ -2,7 +2,32 @@
 
 class shopMlmPluginSettingsAction extends waViewAction
 {
+    protected $plugin_id = array('shop', 'mlm');
+
     public function execute()
     {
+        $templates = array(
+            'signup' => array('name' => _wp('MLM sign up template (signup.html)'), 'tpl_path' => 'plugins/mlm/templates/signup.html', 'public' => false),
+        );
+
+        $app_settings_model = new waAppSettingsModel();
+        $settings = $app_settings_model->get($this->plugin_id);
+
+
+        foreach ($templates as &$template) {
+            $template['full_path'] = wa()->getDataPath($template['tpl_path'], $template['public'], 'shop', true);
+            if (file_exists($template['full_path'])) {
+                $template['change_tpl'] = true;
+            } else {
+                $template['full_path'] = wa()->getAppPath($template['tpl_path'], 'shop');
+                $template['change_tpl'] = false;
+            }
+            $template['template'] = file_get_contents($template['full_path']);
+        }
+        unset($template);
+
+
+        $this->view->assign('settings', $settings);
+        $this->view->assign('templates', $templates);
     }
 }
